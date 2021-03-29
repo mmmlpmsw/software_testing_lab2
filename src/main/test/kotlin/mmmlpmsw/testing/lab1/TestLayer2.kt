5d9fd00593a59aaa6ce954fca45694b5971ef815
mmmlpmsw <mmmlpmsw@gmail.com>
2021-03-29 19:41:21 +0300

    Fix 2nd layer tests

--- src/main/test/kotlin/mmmlpmsw/testing/lab1/TestLayer2.kt
+++ src/main/test/kotlin/mmmlpmsw/testing/lab1/TestLayer2.kt
@@ -173,28 +173,28 @@ class TestLayer2 {
         Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -1.0501)).thenReturn(0.4974843030814569)
         Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -1.0514)).thenReturn(0.49635616815304967)
         Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -1.054)).thenReturn(0.4940973836787822)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.284)).thenReturn(-0.6542599654352104)
+        Mockito.`when`(mock.sin(-PERIOD -2.284 + Math.PI/2)).thenReturn(-0.6542599654352104)
         Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.285)).thenReturn(-0.6550159079046579)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.29)).thenReturn(-0.6587857799181878)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.344)).thenReturn(-0.6984316134723678)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -3 * Math.PI/4)).thenReturn(-0.7071067811865475)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.4)).thenReturn(-0.7373937155412454)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.4227)).thenReturn(-0.752535435303037)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.4401)).thenReturn(-0.7638797417584241)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.5)).thenReturn(-0.8011436155469337)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -2.668)).thenReturn(-0.889935474355517)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -3.0)).thenReturn(-0.9899924966004454)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -3.3)).thenReturn(-0.9874797699088649)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -3.38)).thenReturn(-0.9717153207120621)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -3.542)).thenReturn(-0.920902289427134)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -3.597)).thenReturn(-0.8980819402628435)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -5.497)).thenReturn(0.7065499674778862)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -5.498)).thenReturn(0.7072572772417635)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -5.501)).thenReturn(0.709374960164383)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -5.535)).thenReturn(0.7329246283931939)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -5.537)).thenReturn(0.7342837813330652)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -6.045)).thenReturn(0.9717677325038732)
-        Mockito.`when`(mock.sin(Math.PI/2 -PERIOD -6.05)).thenReturn(0.9729352782968974)
+        Mockito.`when`(mock.sin(-PERIOD -2.29 + Math.PI/2)).thenReturn(-0.6587857799181878)
+        Mockito.`when`(mock.sin(-PERIOD -2.344 + Math.PI/2)).thenReturn(-0.6984316134723678)
+        Mockito.`when`(mock.sin(-PERIOD -3 * Math.PI/4 + Math.PI/2)).thenReturn(-0.7071067811865475)
+        Mockito.`when`(mock.sin( -PERIOD -2.4 + Math.PI/2)).thenReturn(-0.7373937155412454)
+        Mockito.`when`(mock.sin( -PERIOD -2.4227 + Math.PI/2)).thenReturn(-0.752535435303037)
+        Mockito.`when`(mock.sin( -PERIOD -2.4401 + Math.PI/2)).thenReturn(-0.7638797417584241)
+        Mockito.`when`(mock.sin( -PERIOD -2.5 + Math.PI/2)).thenReturn(-0.8011436155469337)
+        Mockito.`when`(mock.sin( -PERIOD -2.668 + Math.PI/2)).thenReturn(-0.889935474355517)
+        Mockito.`when`(mock.sin( -PERIOD -3.0 + Math.PI/2)).thenReturn(-0.9899924966004454)
+        Mockito.`when`(mock.sin( -PERIOD -3.3 + Math.PI/2)).thenReturn(-0.9874797699088649)
+        Mockito.`when`(mock.sin( -PERIOD -3.38 + Math.PI/2)).thenReturn(-0.9717153207120621)
+        Mockito.`when`(mock.sin( -PERIOD -3.542 + Math.PI/2)).thenReturn(-0.920902289427134)
+        Mockito.`when`(mock.sin( -PERIOD -3.597 + Math.PI/2)).thenReturn(-0.8980819402628435)
+        Mockito.`when`(mock.sin( -PERIOD -5.497 + Math.PI/2)).thenReturn(0.7065499674778862)
+        Mockito.`when`(mock.sin( -PERIOD -5.498 + Math.PI/2)).thenReturn(0.7072572772417635)
+        Mockito.`when`(mock.sin( -PERIOD -5.501 + Math.PI/2)).thenReturn(0.709374960164383)
+        Mockito.`when`(mock.sin( -PERIOD -5.535 + Math.PI/2)).thenReturn(0.7329246283931939)
+        Mockito.`when`(mock.sin( -PERIOD -5.537 + Math.PI/2)).thenReturn(0.7342837813330652)
+        Mockito.`when`(mock.sin( -PERIOD -6.045 + Math.PI/2)).thenReturn(0.9717677325038732)
+        Mockito.`when`(mock.sin( -PERIOD -6.05 + Math.PI/2)).thenReturn(0.9729352782968974)
 
 
 //=======================================================================================================
@@ -351,14 +351,13 @@ class TestLayer2 {
         assertAll (
                 { TestUtilities.assertBigDouble(Double.NaN, calculator.calculate(-Math.PI / 2)) },
                 { TestUtilities.assertBigDouble(Double.NEGATIVE_INFINITY, calculator.calculate(-Math.PI / 2 + EPS)) },
-//                { TestUtilities.assertBigDouble(Double.POSITIVE_INFINITY, calculator.calculate(-Math.PI / 2 - EPS)) },
+                { TestUtilities.assertBigDouble(Double.NaN, calculator.calculate(-Math.PI / 2 - EPS)) },
                 { TestUtilities.assertBigDouble(Double.POSITIVE_INFINITY, calculator.calculate(-3 * Math.PI / 2)) },
                 { TestUtilities.assertBigDouble(Double.POSITIVE_INFINITY, calculator.calculate(-3 * Math.PI / 2 + EPS)) },
                 { TestUtilities.assertBigDouble(Double.NEGATIVE_INFINITY, calculator.calculate(-3 * Math.PI / 2 - EPS)) },
                 { TestUtilities.assertBigDouble(Double.NaN, calculator.calculate(-2 * Math.PI)) },
                 { TestUtilities.assertBigDouble(Double.NEGATIVE_INFINITY, calculator.calculate(-2 * Math.PI + EPS)) }
         )
-
     }
 
     @Test
@@ -373,26 +372,25 @@ class TestLayer2 {
         TestUtilities.assertBigDouble(-3.459139462413303E95, calculator.calculate(-PERIOD - 1.0501))
         TestUtilities.assertBigDouble(-2.1586232559181542E95, calculator.calculate(-PERIOD - 1.0514))
         TestUtilities.assertBigDouble(-3.637595146522363E95, calculator.calculate(-PERIOD - 1.054))
-
-//        TestUtilities.assertBigDouble(4698531.583429447, calculator.calculate(-PERIOD - 2.284))
-//        TestUtilities.assertBigDouble(4689757.264512879, calculator.calculate(-PERIOD - 2.285))
-//        TestUtilities.assertBigDouble(4818554.651139655, calculator.calculate(-PERIOD - 2.29))
-//        TestUtilities.assertBigDouble(4.661155269375071E7, calculator.calculate(-PERIOD - 2.344))
-//        Assertions.assertEquals(0.0, calculator.calculate(-PERIOD - 3 * Math.PI / 4), 1E-3)
-//        TestUtilities.assertBigDouble(-1168534.0723719394, calculator.calculate(-PERIOD - 2.4227))
-//        TestUtilities.assertBigDouble(-2.5692678883176365E8, calculator.calculate(-PERIOD - 2.4401))
-//        TestUtilities.assertBigDouble(-1.1820018502257984E7, calculator.calculate(-PERIOD - 2.5))
-//        TestUtilities.assertBigDouble(-7572.567532493296, calculator.calculate(-PERIOD - 2.668))
-//        TestUtilities.assertBigDouble(-1.6274110779708395E11, calculator.calculate(-PERIOD - 3.0))
-//        TestUtilities.assertBigDouble(-2138073.650667048, calculator.calculate(-PERIOD - 3.3))
-//        TestUtilities.assertBigDouble(-8.094330766072657E-15, calculator.calculate(-PERIOD - 3.38))
-//        TestUtilities.assertBigDouble(-28.266518244731415, calculator.calculate(-PERIOD - 3.542))
-//        TestUtilities.assertBigDouble(-9.846278079315185, calculator.calculate(-PERIOD - 3.597))
-//        TestUtilities.assertBigDouble(-6.282401637852932E55, calculator.calculate(-PERIOD - 5.497))
-//        TestUtilities.assertBigDouble(1.2539732706036974E55, calculator.calculate(-PERIOD - 5.498))
-//        TestUtilities.assertBigDouble(7.617779751608887E55, calculator.calculate(-PERIOD - 5.501))
-//        TestUtilities.assertBigDouble(3.140387178975765E52, calculator.calculate(-PERIOD - 5.535))
-//        TestUtilities.assertBigDouble(1.8176701451466236E52, calculator.calculate(-PERIOD - 5.537))
+        TestUtilities.assertBigDouble(4698531.583429447, calculator.calculate(-PERIOD - 2.284))
+        TestUtilities.assertBigDouble(4689757.264512879, calculator.calculate(-PERIOD - 2.285))
+        TestUtilities.assertBigDouble(4818554.651139655, calculator.calculate(-PERIOD - 2.29))
+        TestUtilities.assertBigDouble(4.661155269375071E7, calculator.calculate(-PERIOD - 2.344))
+        Assertions.assertEquals(0.0, calculator.calculate(-PERIOD - 3 * Math.PI / 4), 1E-3)
+        TestUtilities.assertBigDouble(-1168534.0723719394, calculator.calculate(-PERIOD - 2.4227))
+        TestUtilities.assertBigDouble(-2.5692678883176365E8, calculator.calculate(-PERIOD - 2.4401))
+        TestUtilities.assertBigDouble(-1.1820018502257984E7, calculator.calculate(-PERIOD - 2.5))
+        TestUtilities.assertBigDouble(-7572.567532493296, calculator.calculate(-PERIOD - 2.668))
+        TestUtilities.assertBigDouble(-1.6274110779708395E11, calculator.calculate(-PERIOD - 3.0))
+        TestUtilities.assertBigDouble(-2138073.650667048, calculator.calculate(-PERIOD - 3.3))
+        TestUtilities.assertBigDouble(-8.094330766072657E-15, calculator.calculate(-PERIOD - 3.38))
+        TestUtilities.assertBigDouble(-28.266518244731415, calculator.calculate(-PERIOD - 3.542))
+        TestUtilities.assertBigDouble(-9.846278079315185, calculator.calculate(-PERIOD - 3.597))
+        TestUtilities.assertBigDouble(-6.282401637852932E55, calculator.calculate(-PERIOD - 5.497))
+        TestUtilities.assertBigDouble(1.2539732706036974E55, calculator.calculate(-PERIOD - 5.498))
+        TestUtilities.assertBigDouble(7.617779751608887E55, calculator.calculate(-PERIOD - 5.501))
+        TestUtilities.assertBigDouble(3.140387178975765E52, calculator.calculate(-PERIOD - 5.535))
+        TestUtilities.assertBigDouble(1.8176701451466236E52, calculator.calculate(-PERIOD - 5.537))
         TestUtilities.assertBigDouble(-1.1804506174592054E-14, calculator.calculate(-PERIOD - 6.045))
         TestUtilities.assertBigDouble(-5.929431390477587E-5, calculator.calculate(-PERIOD - 6.05))
     }
