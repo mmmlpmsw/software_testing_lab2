f89ccbf30cd8f1b0b3501fdbff88fbf13456c320
mmmlpmsw <mmmlpmsw@gmail.com>
2021-03-29 18:57:31 +0300

    Fix values in test

--- src/main/kotlin/mmmlpmsw/testing/lab1/TestGenerateUtils.kt
+++ src/main/kotlin/mmmlpmsw/testing/lab1/TestGenerateUtils.kt
@@ -3,18 +3,134 @@ package mmmlpmsw.testing.lab1
 import kotlin.math.*
 import kotlin.math.log10
 
-fun main() {
-    val vals = arrayListOf(0.0, -0.00000001, -0.112, -0.5, -0.64, -0.66, -2.25, -2.2818, -2.303, -2.344, -2.3561945, -2.359,
-            -2.422, -2.42270101, -2.4381, -2.509, -2.59999, -2.8, -3.1123781, -3.1637666, -3.66, -3.685426, -3.8,
-            -5.889, -5.95, -6.1789174, -6.28
+fun generateMock1st() {
+    val vals = arrayListOf(
+            -0.19, -0.2, -0.642, //f(x) = 0
+            -0.915, -0.9331,  // local min
+            -0.9351, //f(x) = 0
+            -1.0501, -1.0514, //local max
+            -1.054,
+            -2.284, -2.285, // local min
+            -2.29, -2.344, //local max
+            - 3 * Math.PI/4, -2.4,
+            -2.4227, //f(x) = 0
+            -2.4401, // local min
+            -2.5, -2.668, //local max
+            -3.0, -3.3, -3.38, //f(x) = 0
+            -3.542, -3.597,
+            -5.497, -5.498, //f(x) = 0
+            -5.501, -5.535, -5.537, -6.045, //f(x) = 0
+            -6.05
     )
 
-//    for (v in vals) {
-//        val result = 1/ sin(v)
-//        println("Mockito.`when`(mock.csc(-PERIOD + $v)).thenReturn($result)")
-//    }
+    for (v in vals) {
+        val result = sin(v)
+        println("Mockito.`when`(mock.sin($v)).thenReturn($result)")
+    }
 
-//    val expected = arrayListOf(Double.NaN, -1.562500435646825E110,
+    for (v in vals) {
+        val result = cos(v)
+        println("Mockito.`when`(mock.cos($v)).thenReturn($result)")
+    }
+
+    for (v in vals) {
+        val result = tan(v)
+        println("Mockito.`when`(mock.tan($v)).thenReturn($result)")
+    }
+
+    for (v in vals) {
+        val result = 1/tan(v)
+        println("Mockito.`when`(mock.cot($v)).thenReturn($result)")
+    }
+
+    for (v in vals) {
+        val result = 1/ cos(v)
+        println("Mockito.`when`(mock.sec($v)).thenReturn($result)")
+    }
+
+    for (v in vals) {
+        val result = 1/ sin(v)
+        println("Mockito.`when`(mock.csc($v)).thenReturn($result)")
+    }
+
+}
+
+fun generateTests1st() {
+    val vals = arrayListOf(
+            -0.19, -0.2, -0.642, //f(x) = 0
+            -0.915, -0.9331,  // local min
+            -0.9351, //f(x) = 0
+            -1.0501, -1.0514, //local max
+            -1.054,
+            -2.284, -2.285, // local min
+            -2.29, -2.344, //local max
+//            - 3 * Math.PI/4, -2.4,
+            -2.4227, //f(x) = 0
+            -2.4401, // local min
+            -2.5, -2.668, //local max
+            -3.0, -3.3, -3.38, //f(x) = 0
+            -3.542, -3.597,
+            -5.497, -5.498, //f(x) = 0
+            -5.501, -5.535, -5.537, -6.045, //f(x) = 0
+            -6.05
+    )
+
+    val originalMath = OriginalMath()
+    for (v in vals) {
+        val result = originalMath.calculateOrigin(v)
+        println("assertBigDouble($result, calculator.calculate($v))")
+    }
+
+}
+
+fun generateForAsymptotes1st() {
+    val EPS = 1e-8
+    val values = arrayListOf( 0.0, 0.0 - EPS, -Math.PI/2, -Math.PI/2 - EPS, -Math.PI/2 + EPS,
+            -Math.PI, -Math.PI - EPS, -Math.PI + EPS,
+            -3 * Math.PI/2, -3 * Math.PI/2 - EPS, -3 * Math.PI/2 + EPS,
+            -2 * PI + EPS
+    )
+
+    val valuesStr = arrayListOf( "0.0", "0.0 - EPS", "-Math.PI/2", "-Math.PI/2 - EPS", "-Math.PI/2 + EPS",
+            "-Math.PI", "-Math.PI - EPS", "-Math.PI + EPS",
+            "-3 * Math.PI/2", "-3 * Math.PI/2 - EPS", "-3 * Math.PI/2 + EPS",
+            "-2 * PI + EPS"
+    )
+
+    for ((i, v) in values.withIndex()) {
+        val result = sin(v)
+        println("Mockito.`when`(mock.sin(${valuesStr[i]})).thenReturn($result)")
+    }
+
+    for ((i, v) in values.withIndex()) {
+        val result = cos(v)
+        println("Mockito.`when`(mock.cos(${valuesStr[i]})).thenReturn($result)")
+    }
+
+    for ((i, v) in values.withIndex()) {
+        val result = tan(v)
+        println("Mockito.`when`(mock.tan(${valuesStr[i]})).thenReturn($result)")
+    }
+
+    for ((i, v) in values.withIndex()) {
+        val result = 1/tan(v)
+        println("Mockito.`when`(mock.cot(${valuesStr[i]})).thenReturn($result)")
+    }
+
+    for ((i, v) in values.withIndex()) {
+        val result = 1/cos(v)
+        println("Mockito.`when`(mock.sec(${valuesStr[i]})).thenReturn($result)")
+    }
+
+    for ((i, v) in values.withIndex()) {
+        val result = 1/sin(v)
+        println("Mockito.`when`(mock.csc(${valuesStr[i]})).thenReturn($result)")
+    }
+
+}
+
+fun generateLogMock1st() {
+    //    val expected = arrayListOf(Double.NaN, -1.562500435646825E110,
 //            -7.199400495612413E12, -210297.749052139, -1757.8922042876907, -1.869931113367555E7, 1.4542650843086908E7,
 //            4756791.712747779, 6892356.123178437, 4.661155269375071E7, -92.41824433050677, -3.1853823000403974E7,
 //            2.610368742533695E7, -1206454.380274456, -2.5475768135351714E8, -5500786.236013713, -2191.8274562229167,
@@ -38,4 +154,8 @@ fun main() {
 //    for (i in logVals.indices) {
 //        println("assertEquals(" + logExpected[i] + ", calculator.calculate(" + logVals[i] + "), 1E-3)")
 //    }
+}
+
+fun main() {
+
 }
